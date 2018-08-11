3feac373504cdd2677db9da71987e3fb00ea66de
Richard Nghiem <richard@nghiem.org>
2018-08-11 20:24:16 +0900

    server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+var NAME = process.env.NAME || 'world';
+
+app.get('/', function (req, res) {
+  res.send(`hello ${NAME}!`);
+});
+
+app.listen(PORT, function () {
+  console.log(`Server is listening on port ${PORT}`);
+});
