e19acd6227aaff6f044693fa6bb6d8f25e05cc29
Richard Nghiem <richard@nghiem.org>
2018-08-30 18:03:49 +0800

    print env vars: NAME, PORT, and NODE_ENV

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ var app = express();
 
 var PORT = process.env.PORT || 3000;
 var NAME = process.env.NAME || 'world';
+var NODE_ENV = process.env.NODE_ENV || 'xxx';
 
 app.get('/', function (req, res) {
-  res.send(`hello ${NAME}!`);
+  res.write(`hello ${NAME}!\n\n`);
+  res.write(`name: ${NAME}\n`);
+  res.write(`port: ${PORT}\n`);
+  res.write(`env:  ${NODE_ENV}\n`);
+  res.end();
 });
 
 app.listen(PORT, function () {
